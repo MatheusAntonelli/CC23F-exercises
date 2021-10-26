d10e3d12f64bb07045c0024077df30001ce5b6c2
Matheus Antonelli <matheusantonelli12@gmail.com>
2021-10-26 00:30:49 -0300

    route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Atv2 from './views/Atv2';
@@ -17,25 +17,4 @@ function Routes() {
     )
 }
 
-export default Routes
-=======
-import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Atv2 from './views/Atv2';
-import MainPage from './views/MainPage';
-
-function Routes() {
-    return (
-        <>
-            <Router>
-                <Switch>
-                    <Route path='/' exact component={MainPage}/>
-                    <Route path='/atividade-2' exact component={Atv2}/>
-                </Switch>
-            </Router>
-        </>
-    )
-}
-
-export default Routes
->>>>>>> b93b3d58a017db4a6691477c908add7f1b784338
+export default Routes;
---
+++ src/components/Atv1/Exercise1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Exercise1() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Exercise1
