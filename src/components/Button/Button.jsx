2e9ac143d0836700db3fe67538568b6b22500244
Matheus Antonelli <matheusantonelli12@gmail.com>
2021-10-25 21:59:59 -0300

    Button style and component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import './Button.css';
 
-function Button() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const STYLES = ['btn-primary', 'btn-outline'];
 
-export default Button
+const SIZES = ['btn-medium', 'btn-large'];
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize,
+    
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize: SIZES[0];
+
+    return(
+
+            <button
+                className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+                onClick={onClick}
+                type={type}
+                >
+                {children}
+            </button>
+
+    );
+};
